fc5b6b80bcc63a4362eb7d9af0999ca36d30f9c0
Seungho Choi <ksngho7@gmail.com>
2018-07-22 22:03:28 +0900

    add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,35 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp' , { useNewUrlParser: true }, (err, client) => {
+  if(err){
+    return console.log('Unable to connect to MongoDB server')
+  }
+  console.log('connected to mongo baby');
+  var db= client.db('TodoApp');
+
+  // // deleteMany
+  // db.collection('Todos').deleteMany({text:'sexy man'})
+  //   .then((result)=>{
+  //     console.log(result);
+  //   })
+  // // deleteOne
+  // db.collection('Todos').deleteOne({text:'swag'})
+  //   .then((result)=>{
+  //     console.log(result.result);
+  //   })
+  // // findOneAndDelete
+  // db.collection('Todos').findOneAndDelete({completed:true})
+  //   .then((result)=>{
+  //     console.log(result);
+  //   });
+  db.collection('Users').deleteMany({name:'Sean'})
+    .then(res=>{
+      console.log(res);
+    })
+
+
+
+  // client.close();
+});
